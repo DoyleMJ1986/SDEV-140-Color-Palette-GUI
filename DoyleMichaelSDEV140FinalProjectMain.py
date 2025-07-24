a5b008bf050e11ad20e6ec4a5df59eee83e95aec
DoyleMJ1986 <mdoyle48@ivytech.edu>
2025-07-24 14:40:17 -0400

    Add files via upload

--- DoyleMichaelSDEV140FinalProjectMain.py
+++ DoyleMichaelSDEV140FinalProjectMain.py
@@ -19,92 +19,92 @@ class ColorPaletteApp(tk.Tk):
         # Red Component
         self.redLabel = tk.Label(self, text="Red Value", fg="red")
         self.redLabel.configure(bg="#a8e4a0")
-        self.redLabel.pack(side='top', anchor='s')
+        self.redLabel.grid(row=0, column=0, sticky="nsew")
         self.redEntryLabel = tk.Label(self, text="Enter Red Value Here")
-        self.redEntryLabel.pack(side='top', fill='x')
+        self.redEntryLabel.grid(row=1, column=0, sticky="nsew")
         self.redEntry = tk.Entry(self, state = "normal")
-        self.redEntry.pack(side='top', fill='x')
+        self.redEntry.grid(row=2, column=0, sticky="nsew")
         self.redEntryButton = tk.Button(self, text="Set Red", command=self.setRed)
-        self.redEntryButton.pack(side='top', fill='x')
+        self.redEntryButton.grid(row=3, column=0, sticky="nsew")
         self.redSlider = tk.Scale(self, from_=0, to=255, orient="horizontal", command=self.calculateRGB)
         self.redSlider.configure(bg="#a8e4a0")
-        self.redSlider.pack(side='top', anchor='s')
+        self.redSlider.grid(row=4, column=0, sticky="nsew")
 
         # Green Component
         self.greenLabel = tk.Label(self, text="Green Value", fg="green")
-        self.greenLabel.pack(side='left')
+        self.greenLabel.grid(row=0, column=1, sticky="nsew")
         self.greenLabel.configure(bg="#a8e4a0")
         self.greenEntryLabel =tk.Label(self, text="Enter Green Value Here")
-        self.greenEntryLabel.pack(side='left', fill = 'x')
+        self.greenEntryLabel.grid(row=1, column=1, sticky="nsew")
         self.greenEntry = tk.Entry(self, state = "normal")
+        self.greenEntry.grid(row=2, column=1, sticky="nsew")
         self.greenEntryButton = tk.Button(self, text="Set Green", command=self.setGreen)
-        self.greenEntryButton.pack(side='left')
+        self.greenEntryButton.grid(row=3, column=1, sticky="nsew")
         self.greenSlider = tk.Scale(self, from_=0, to=255, orient='horizontal', command=self.calculateRGB)
         self.greenSlider.configure(bg="#a8e4a0")
-        self.greenSlider.pack(side='left')
+        self.greenSlider.grid(row=4, column=1, sticky="nsew")
 
         # Blue Component
         self.blueLabel = tk.Label(self, text="Blue Value", fg="blue")
-        self.blueLabel.pack(side='right')
+        self.blueLabel.grid(row=0, column=2, sticky="nsew")
         self.blueEntryLabel = tk.Label(self, text="Enter Blue Value Here")
-        self.blueEntryLabel.pack(side='right')
+        self.blueEntryLabel.grid(row=1, column=2, sticky="nsew")
         self.blueEntry = tk.Entry(self, state = "normal")
-        self.blueEntry.pack(side='right')
+        self.blueEntry.grid(row=2, column=2, sticky="nsew")
         self.blueEntrybutton = tk.Button(self, text="Set Blue", command=self.setBlue)
-        self.blueEntrybutton.pack(side='right')
+        self.blueEntrybutton.grid(row=3, column=2, sticky="nsew")
         self.blueSlider = tk.Scale(self, from_=0, to=255, orient='horizontal', command=self.calculateRGB)
-        self.blueSlider.pack(side='right')
+        self.blueSlider.grid(row=4, column=2, sticky="nsew")
 
         # Color Display Canvas
         self.canvasLabel = tk.Label(self, text="Curent Color")
-        self.canvasLabel.pack(side='top', anchor ='n', fill='x')
+        self.canvasLabel.grid(row=5, column=5, sticky="nsew")
         self.myCanvas = tk.Canvas(self, width=100, height=100, background="black")
-        self.myCanvas.pack(side='top')
+        self.myCanvas.grid(row=6, column=5, sticky="nsew")
 
         # RGB Code Display
         self.rgbLabel = tk.Label(self, text="RGB Value")
-        self.rgbLabel.pack(side='top', anchor='n', fill='x')
+        self.rgbLabel.grid(row=10, column=5, sticky="nsew")
         self.rgbD = tk.Entry(self, state="readonly")
         self.rgbD.configure(bg="white")
-        self.rgbD.pack(side='top')
+        self.rgbD.grid(row=11, column=5, sticky="nsew")
         # RGB Copy Button
         self.cpyRGB = tk.Button(self, text= "Copy Code", command=self.copyRGB)
-        self.cpyRGB.pack(side='top', anchor='n', fill='x')
+        self.cpyRGB.grid(row=12, column=5, sticky="nsew")
 
         # HSV Code Display
         self.hsvLabel = tk.Label(self, text="HSV Value")
-        self.hsvLabel.pack(side='left', fill='x')
+        self.hsvLabel.grid(row=13, column=5, sticky="nsew")
         self.hsvD = tk.Entry(self, state="readonly")
         self.hsvD.configure(bg="white")
-        self.hsvD.configure(bg="white")
-        self.hsvD.pack(side='left', fill='x')
+        self.hsvD.grid(row=13, column=5, sticky="nsew")
         # HSV Copy Button
-        self.hsvCpy = (tk.Button(self, text= "Copy Code", command=self.copyHSV))
+        self.hsvCpy = tk.Button(self, text= "Copy Code", command=self.copyHSV)
         self.hsvCpy.configure(bg="white")
-        self.hsvCpy.pack(side='left', fill='x')
+        self.hsvCpy.grid(row=14, column=5, sticky="nsew")
 
         # CYMK Code Display
         self.cymkLabel = tk.Label(self, text="CYMK Value")
-        self.cymkLabel.pack(side='right', fill='x')
+        self.cymkLabel.grid(row=15, column=5, sticky="nsew")
         self.cymkD = tk.Entry(self, state="readonly")
         self.cymkD.configure(bg="white")
-        self.cymkD.pack(side='right', fill='x')
+        self.cymkD.grid(row=16, column=5, sticky="nsew")
         #CYMK Clipboard Copy Button
         self.cymkCpy = tk.Button(self, text="Copy Code", command=self.copyCYMK)
         self.cymkCpy.configure(bg="white")
-        self.cymkCpy.pack(side='right', fill='x')
+        self.cymkCpy.grid(row=17, column=5, sticky="nsew")
         # RGB Inverter Button
         self.RGBInvLabel = tk.Label(self, text="RGB Value Invert")
-        self.RGBInvLabel.pack(side='left', fill='x')
+        self.RGBInvLabel.grid(row=18, column=5, sticky="nsew")
         # HSV Interver Button
         self.HSVInvLabel = tk.Label(self, text="HSV Value Invert")
-        self.HSVInvLabel.pack(side='top', fill='x')
+        self.HSVInvLabel.grid(row=19, column=5, sticky="nsew")
         # CYMK Inverter Button
         self.CYMKInvLabel = tk.Label(self, text="CYMK Value Invert")
-        self.CYMKInvLabel.pack(side='right', fill='x')
+        self.CYMKInvLabel.grid(row=20, column=5, sticky="nsew")
         # Quit Button
         self.quitButton = tk.Button(self, text="Quit", command=self.quit)
-        self.quitButton.pack(side='bottom', anchor='s')
+        self.quitButton.grid(row=21, column=5, sticky="nsew")
 
     # Calculate RGB Hex Code
     def calculateRGB(self, event):
@@ -118,7 +118,7 @@ class ColorPaletteApp(tk.Tk):
         self.rgbD.configure(state='normal')
         self.rgbD.delete(0, tk.END)
         self.rgbD.insert(0, hexColor)
-        self.hsvD.configure(state='readonly')
+        self.rgbD.configure(state='readonly')
 
     # Get Value for Red from entry box and transfer it to the slider
     def setRed(self):
@@ -147,7 +147,7 @@ class ColorPaletteApp(tk.Tk):
                 raise ValueError
             else:
                 intG = int(g)
-                if intG < 0 or intG > 255
+                if intG < 0 or intG > 255:
                     raise ValueError
                 else:
                 # Set the Value on the Green Slider to the value entered here
@@ -155,23 +155,25 @@ class ColorPaletteApp(tk.Tk):
         except ValueError:
             self.greenValueError = tk.Message(self, text= "ERROR! Please enter a numbner from 0 to 255")
 
-    # Get Value for Green from entry box and transfer it to the slider
+    # Get Value for Blue from entry box and transfer it to the slider
     def setBlue(self):
         try:
             b = self.blueEntry.get()
             # Accept Numerical Values fromn 0 to 255 only
             if not b.isdigit():
                 raise ValueError
-            elif b < 0 or b > 255:
-                raise ValueError
             else:
-                # Set the value on the Green Slider to the value entered here
-                self.greenSlider.set(intB)
+                intB = int(b)
+                if intB < 0 or intB > 255:
+                    raise ValueError
+                else:
+                    # Set the value on the Green Slider to the value entered here
+                    self.blueSlider.set(intB)
         except ValueError:
             self.blueValueError = tk.Message(self, text= "ERROR! Please enter a numbner from 0 to 255")
 
         ## HSV Code Generation ##
-    def calculateHSV(self, r, g, b):
+    def calculateHSV(self):
         # Used in calculations for (hue, saturation, value) later
         colorHue = 0
         colorSaturation = 0
@@ -206,9 +208,13 @@ class ColorPaletteApp(tk.Tk):
 
         # Value Calculations (colorValue)
         colorValue = colorMax * 100
-        HSVValue = f"{colorHue:2x} {colorSaturation:2x} {colorValue:2x}"
-        self.myCanvas.config(bg=HSVValue)
-    def calculateCYMK(self, r, g, b):
+        HSVValue = f"{colorHue}, {colorSaturation}, {colorValue}"
+        self.hsvD.configure(state='normal')
+        self.hsvD.delete(0, tk.END)
+        self.hsvD.insert(0, HSVValue)
+        self.hsvD.configure(state='readonly')
+
+    def calculateCYMK(self):
         r = self.redSlider.get()
         g = self.greenSlider.get()
         b = self.blueSlider.get()
@@ -221,6 +227,7 @@ class ColorPaletteApp(tk.Tk):
         rNorm = r / 255.0
         gNorm = g / 255.0
         bNorm = b / 255.0
+
 
         # Black Value. This is whichever of the normlaized values is greatest
         k = 1 - max(rNorm, gNorm, bNorm)
@@ -245,10 +252,14 @@ class ColorPaletteApp(tk.Tk):
 
         # Convert above totals to percentages, rounded to nearest whole numbers
         c = round(c * 100)
-        m = round(m * 100)
-        y = round(y * 100)
+        y = round(m * 100)
+        m = round(y * 100)
         k = round(k * 100)
-
+        cymkValue = f"({c},{y},{m},{k})"
+        cymkD.configure(state='normal')
+        cymkD.delete(0, tk.END)
+        cymkD.insert(0, cymkValue)
+        cymkD.configure(state='readonly')
     # Copy RGB to Clipboard
     def copyRGB(self):
         rgbc = self.rgbD.get()
@@ -262,7 +273,7 @@ class ColorPaletteApp(tk.Tk):
         cymkc = self.cymkD.get()
         self.clipboard_append(cymkc)
     # RGB Inversion
-    def rgbInvert(self,r, g, b):
+    def rgbInvert(self):
         # Initial Values
         r = self.redSlider.get()
         intR = int(r)
@@ -276,6 +287,9 @@ class ColorPaletteApp(tk.Tk):
         invG = 255 - intG
         invB = 255 - intB
 
+        redSlider.set(invR)
+        greenSlider.set(invG)
+        blueSlider.set(invB)
 
 
     # HSV Inversion
@@ -283,6 +297,7 @@ class ColorPaletteApp(tk.Tk):
         r = self.redSlider.get()
         g = self.greenSlider.get()
         b = self.blueSlider.get()
+
     #CYMK Inversion
     def cymkInvert(self):
         r = self.redSlider.get()
